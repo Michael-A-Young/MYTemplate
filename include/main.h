db14b70627ead587634642bf4edf4651b4f32916
Michael Young <YoungM0202@outlook.com>
2026-08-31 16:39:19 +0100

    Add args to main function for completion.

--- include/main.h
+++ include/main.h
@@ -1,6 +1,6 @@
 #ifndef MYTEMPLATE_MAIN_H
 #define MYTEMPLATE_MAIN_H
 
-int main();
+int main(int argc, char** argv);
 
 #endif // MYTEMPLATE_MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 // Standard Library
 #include <iostream>
 
-int main()
+int main(int /* argc */, char** /* argv */)
 {
 	std::cout << "Hello, world!\n";
 	return 0;
